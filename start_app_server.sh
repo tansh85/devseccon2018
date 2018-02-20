9f96b37b374ff44292ece3cdd8bf2f40c3a58f4e
Siang Hui <tan_siang_hui@tech.gov.sg>
2018-02-20 17:29:33 +0800

    amended mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://fracture_user:nopass00@ds243728.mlab.com:43728/fracture
+export MONGODB_CREDENTIALS=mongodb://fracture_user:noname00@ds243728.mlab.com:43728/fracture
 
 # finally, run the app in dev mode...
 npm run dev
