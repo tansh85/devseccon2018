814af3ced5cb89423eb8e31ad844bbc1c7e9863e
Siang Hui <tan_siang_hui@tech.gov.sg>
2018-02-20 17:37:27 +0800

    removed mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://fact_user:qqqq1111@ds243728.mlab.com:43728/fracture
+#export MONGODB_CREDENTIALS=mongodb://fact_user:qqqq1111@ds243728.mlab.com:43728/fracture
 
 # finally, run the app in dev mode...
 npm run dev
